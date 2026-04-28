4caf1b4b3fb9523c21dda9801285ccbca8151f78
FikryIdhamD <147605722+FikryIdhamD@users.noreply.github.com>
2026-04-28 14:03:31 +0700

    feat: setup clean architecture folders and app theme

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF121212); // BG Utama Spotify
+  static const Color cardGrey = Color(0xFF1E1E1E); // BG untuk List Tile
+  static const Color green = Color(0xFF1DB954); // Hijau Spotify
+  static const Color lightGrey = Color(0xFFB3B3B3); // Text secondary
+  static const Color white = Colors.white;
+}
